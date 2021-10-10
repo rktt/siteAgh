4413cc197f7483eab8e172434c40f579aaa2a850
rktt <85611044+rktt@users.noreply.github.com>
2021-10-10 05:40:10 -0300

    Update teste-grid.js

--- teste-grid.js
+++ teste-grid.js
@@ -1,59 +1,53 @@
+/*===========================================*/
+let ligado = true;
 
-/* -------------Menu Hamburger--------------*/
-const btnMobile = document.getElementById('btn-mobile');
+const seçaoMenu = document.querySelector('.seçao-Menu')
+const menuAlternar = seçaoMenu.querySelector('.menuAlternancia')
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+menuAlternar.addEventListener('click', () => {
 
-function toggleMenu(event) {
-	if (event.type === 'touchstart') event.preventDefault();
-	const nav = document.getElementById('nav');
-	nav.classList.toggle('active');
+	document.body.style.overflow = ligado ? 'hidden' : 'initial'
 
-	const active = nav.classList.contains('active');
-	event.currentTarget.setAttribute('aria-expanded', 'active');
+	seçaoMenu.classList.toggle('on', ligado)
+	ligado = !ligado;
+})
 
-	if (active) {
-		event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-		} else {
-		event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-		}
-}
-
-/* -----------Validação do formulario e-mail------------*/
+/*===========================================*/
 function validar() {
 	var nome = frmEnvia.name.value;
 	var email = frmEnvia.email.value;
 	var assunto = frmEnvia.subject.value;
 	var mensagem = frmEnvia.message.value;
-	
-	if(nome == "" || nome.length < 3) {
+
+	if (nome == "" || nome.length < 3) {
 		alert('Preencha o campo nome');
 		frmEnvia.name.focus();
 		return false;
 	}
 
-    if(email == "" || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+	if (email == "" || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
 		alert('Preencha o campo com um e-mail válido');
 		frmEnvia.email.focus();
 		return false;
-        }
-		
-	if(assunto == "") {
+	}
+
+	if (assunto == "") {
 		alert('Preencha o campo assunto');
 		frmEnvia.subject.focus();
 		return false;
 	}
 
-    if(mensagem == "" || mensagem.length <= 10) {
+	if (mensagem == "" || mensagem.length <= 10) {
 		alert('Preencha no mínimo com 20(vinte) caracteres');
 		frmEnvia.message.focus();
 		return false;
-	} 
-    
-    else {
-        alert('Sua mensagem foi enviada com sucesso');
+	}
+
+	else {
+		alert('Sua mensagem foi enviada com sucesso');
 		return true;
-    }
+	}
 }
-/* -----------------F I M-----------------*/
+
+
+/*==================== F I M =======================*/
